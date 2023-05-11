d5e7d63764fd86e7215ea48aa71fa6d94400b067
Nile Okomo <okomonile3@gmail.com>
2023-05-11 12:01:29 +0300

    Print error message if user doesn't give file or user gives more than one file

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "monty.h"
+
+/**
+ * main - Program's entry point
+ *
+ * @argc: Argument count
+ * @argv: Arguments
+ *
+ * Return: 0 on success, otherwise, EXIT_FAILURE
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 2 || argc > 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	} else
+	{
+		printf("Yaay!, one argument: %s\n", argv[1]);
+	}
+
+	return (0);
+}
