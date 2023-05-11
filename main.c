42b53cdd1d1f4294d26de06c89a8053562c34fc4
Nile Okomo <okomonile3@gmail.com>
2023-05-11 12:23:21 +0300

    If file cannot open, print error message

--- main.c
+++ main.c
@@ -12,13 +12,30 @@
  */
 int main(int argc, char *argv[])
 {
+	FILE *bytefile;
+
+	/* Check for argument count */
 	if (argc < 2 || argc > 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Yaay!, one argument: %s\n", argv[1]);
+	/*
+	 * Try opening the file, if for any reason file doesn't
+	 * open, print an error to stderr
+	 */
+	bytefile = fopen(argv[1], "r");
+
+	if (bytefile == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("File opens successfully\n");
+
+	fclose(bytefile);
 
 	return (0);
 }
